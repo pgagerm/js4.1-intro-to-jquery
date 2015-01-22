9a0de3c78c5ea81a76611755c2f612cb4b6f1bd2
Michael Germany <mike@rickgermany.com>
2015-01-22 15:37:33 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+//JS4.1: Intro to jQuery
+
+// $(document).on('ready', function() {
+//   $("button").click(function() {
+//   	console.log("Printed message on click");
+//   });
+// });
+
+
+
+// $(document).on('ready', function() {
+//   $("button").click(function() {
+//   	$("body").append("Appended text");
+//   });
+// });
+
+
+
+// $(document).on('ready', function() {
+//   $(".button1").click(function() {
+//   	$("body").append("Appended text");
+//   });
+// $(".button2").click(function() {
+//   	$("body").append("<h1>More appended text</h1>");
+//   });
+// });
+
+
+

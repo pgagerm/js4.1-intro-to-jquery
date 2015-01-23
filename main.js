cd954c0f54fd448339f106344a485d9c2e4660b3
Michael Germany <mike@rickgermany.com>
2015-01-22 17:38:13 -0700

    2nd draft

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 //JS4.1: Intro to jQuery
 
+
+
+// Part I
+
 // $(document).on('ready', function() {
 //   $("button").click(function() {
 //   	console.log("Printed message on click");
@@ -24,6 +28,59 @@
 //   	$("body").append("<h1>More appended text</h1>");
 //   });
 // });
+
+
+
+// $(document).on('ready', function() {
+// 	$(".button1").click(function() {
+//   		$("body").append("Appended text");
+//   	});
+// $(".button2").click(function() {
+// 	$("body").append("<h1>More appended text</h1>");
+//    	});
+// $(".button3").click(function() {
+// 	$("body").append("<ul>header list</ul>", "<li>first item</li>",
+//   	"<li>second item</li>", "<li>third item</li>");
+// 	});
+// });
+
+
+
+// Part II
+
+// $(document).on('ready', function() {
+//   	$('.pink').mouseover(function() {
+//   		$(this).css('color', 'peachpuff');
+//   	});
+// });
+
+
+
+// $(document).on('ready', function() {
+//   	$('.pink').mouseover(function() {
+//   		$(this).css('color', 'peachpuff');
+//   	});
+//   	$('h1').mouseover(function() {
+//   		$(this).append("!");
+//   	});
+// });
+
+
+
+$(document).on('ready', function() {
+  	$('a').on("click", function() {
+  		var answer = confirm("Are you sure you want to navigate to another window?");
+  			if (answer === false) {
+  				event.preventdefault();
+  				return false;
+  			}
+  	});
+});
+
+
+
+
+
 
 
 
